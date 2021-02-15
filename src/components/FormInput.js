be5206a7ca940eea2c3402d3dd892b4919fdac4c
ferddev21 <ferdian.0571@students.amikom.ac.id>
2021-02-15 18:43:38 +0700

    setup project

---
+++ src/components/FormInput.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class FormInput extends React.Component {
+  render() {
+    return (
+      <div>
+        <div>Form Input</div>
+      </div>
+    );
+  }
+}
+
+export default FormInput;
---
+++ src/components/TodoItem.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const TodoItem = () => {
+  return <div>To Do</div>;
+};
+
+export default TodoItem;
